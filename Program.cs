bf446e60b1e7f2c9cb70d00e5eba86fa8946259f
Anna Springfield <anna.e.springfield@gmail.com>
2017-06-07 20:50:45 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace to_do
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your to-do app is running!");
+            Console.WriteLine("Better go catch it ;)");
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Task
+{
+    public string Name;
+    public string Description;
+    // public DateTime DueDate;
+    public bool Completed;
+}
